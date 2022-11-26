1face3d8bd1954765a4ba17dd82fc9aebce6186b
lewisc99 <lewiscontato99@gmail.com>
2022-11-26 10:49:46 -0300

    adding header to the kafka consumer one part 1

--- consumer/src/main/java/com/lewis/consumer/listener/TestListener.java
+++ consumer/src/main/java/com/lewis/consumer/listener/TestListener.java
@@ -2,14 +2,17 @@ package com.lewis.consumer.listener;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 
 
 @Configuration
 public class TestListener {
 
     @KafkaListener(topics = "topic-1",groupId = "group-1")
-    public void Listen(String message)
+    public void Listen(String message, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+                       @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition)
     {
-       System.out.println("Received: " + message);
+       System.out.println("Topic " + topic + " partition: " + partition + " Message: " + message);
     }
 }
