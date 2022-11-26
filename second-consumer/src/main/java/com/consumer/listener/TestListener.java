a0b1e21973fb74f361156503a9577c1d6785646e
lewisc99 <lewiscontato99@gmail.com>
2022-11-26 08:43:22 -0300

    project second consumer added listener kafka file

---
+++ second-consumer/src/main/java/com/consumer/listener/TestListener.java
@@ -0,0 +1,15 @@
+package com.consumer.listener;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+
+
+@Configuration
+public class TestListener {
+
+    @KafkaListener(topics = "topic-1",groupId = "group-1")
+    public void Listen(String message)
+    {
+       System.out.println("Received: " + message);
+    }
+}
